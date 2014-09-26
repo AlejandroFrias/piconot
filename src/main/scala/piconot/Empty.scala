a76a31f00685e893cefc76865876744baf6c0b03
Alejandro Frias <afrias@g.hmc.edu>
2014-09-26 10:15:39 -0700

    Fixed issues with JFXApp by seperating it to a new objet for running it

--- src/main/scala/piconot/Empty.scala
+++ src/main/scala/piconot/Empty.scala
@@ -1,10 +1,49 @@
 package piconot
 
-object Empty extends FriasMarklynAPI {
+import scala.language.postfixOps
 
-    def test = {
-        Face left;
-        Face up;
-        println(rules)
-    }
+import java.io.File
+
+import picolib.maze.Maze
+import picolib.semantics.Anything
+import picolib.semantics.Blocked
+import picolib.semantics.East
+import picolib.semantics.GUIDisplay
+import picolib.semantics.North
+import picolib.semantics.Open
+import picolib.semantics.Picobot
+import picolib.semantics.Rule
+import picolib.semantics.South
+import picolib.semantics.State
+import picolib.semantics.StayHere
+import picolib.semantics.Surroundings
+import picolib.semantics.TextDisplay
+import picolib.semantics.West
+import scalafx.application.JFXApp
+
+
+
+object EmptyRules extends FriasMarklynAPI {
+
+    Sections("start", "Spin");
+
+    New Section "start";
+        Face down;
+        Do Section "Spin"
+
+    New Section "Spin";
+        Turn right;
+        Do Section "Spin";
+
+}
+
+object Empty extends JFXApp{
+
+
+     val emptyMaze = Maze("resources" + File.separator + "empty.txt")
+
+     object EmptyBot extends Picobot(emptyMaze, EmptyRules.rules)
+         with TextDisplay with GUIDisplay
+
+     stage = EmptyBot.mainStage
 }
--- src/main/scala/piconot/FriasMarklynAPI.scala
+++ src/main/scala/piconot/FriasMarklynAPI.scala
@@ -22,11 +22,25 @@ import scalafx.application.JFXApp
 
 
 class FriasMarklynAPI {
-    var section_number = 1
+    var section_number = 0
     var line_number = 0
     var rules: List[Rule] = List() //TODO so ugly
 
-    def genFace(toFace: Int): Unit = {
+    var sections: List[String] = List()
+    
+    def Sections(args: String*): Unit = {
+        sections = args.toList
+    }
+
+
+    trait FaceTrait {
+        def up = genFace(1)
+        def right = genFace(2)
+        def down = genFace(3)
+        def left = genFace(4)
+
+        def genFace(toFace: Int): Unit = {
+        line_number = line_number + 1
         rules = rules ++ List(
             Rule(State(section_number + "0" + line_number + "01"),
                 Surroundings(Anything, Anything, Anything, Anything),
@@ -50,79 +64,87 @@ class FriasMarklynAPI {
                 )
             )
         }
-
-    class FaceClass(val section_number: Int, val line_number: Int) {
-        def up = genFace(1)
-        def right = genFace(2)
-        def down = genFace(3)
-        def left = genFace(4)
     }
 
-    def Face = {
-        line_number = line_number + 1 //TODO so ugly
-        new FaceClass(section_number, line_number)
+    object Face extends FaceTrait
+
+    trait TurnTrait {
+        def left = genTurn(-1)
+        def right = genTurn(1)
+        def around = genTurn(2)
+
+        def genTurn(turnAmount: Int): Unit = {
+            line_number = line_number + 1;
+            rules = rules ++ List(
+                Rule(State(section_number + "0" + line_number + "01"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(section_number + "0" + (line_number + 1)+ "0" + toDir(1 + turnAmount))
+                    ),
+                Rule(State(section_number + "0" + line_number + "02"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(section_number + "0" + (line_number + 1)+ "0" + toDir(2 + turnAmount))
+                    ),
+                Rule(State(section_number + "0" + line_number + "03"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(section_number + "0" + (line_number + 1)+ "0" + toDir(3 + turnAmount))
+                    ),
+                Rule(State(section_number + "0" + line_number + "04"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(section_number + "0" + (line_number + 1)+ "0" + toDir(4 + turnAmount))
+                    )
+                )
+        }
     }
-}
 
-abstract class Command(val section_number: Int, val line_number: Int) {
-    
-}
+    object Turn extends TurnTrait
 
-// trait FaceTrait {
+    def toDir(in: Int) = ((in - 1) % 4) + 1
 
-//     def genFace(toFace: Int) : List[Rule] = List(
-//             Rule(State(section_number + "0" + line_number + "01"),
-//                 Surroundings(Anything, Anything, Anything, Anything),
-//                 North,
-//                 StayHere,
-//                 State(section_number + "0" + (line_number + 1)+ "0" + toFace)
-//                 ),
-//             Rule(State(section_number + "0" + line_number + "02"),
-//                 Surroundings(Anything, Anything, Anything, Anything),
-//                 North,
-//                 StayHere,
-//                 State(section_number + "0" + (line_number + 1)+ "0" + toFace)
-//                 ),
-//             Rule(State(section_number + "0" + line_number + "03"),
-//                 Surroundings(Anything, Anything, Anything, Anything),
-//                 North,
-//                 StayHere,
-//                 State(section_number + "0" + (line_number + 1)+ "0" + toFace)
-//                 ),
-//             Rule(State(section_number + "0" + line_number + "04"),
-//                 Surroundings(Anything, Anything, Anything, Anything),
-//                 North,
-//                 StayHere,
-//                 State(section_number + "0" + (line_number + 1)+ "0" + toFace)
-//                 ))
 
-//     class FaceClass(val section_number: Int, val line_number: Int) {
-//         def up = genFace(1)
-//         def right = genFace(2)
-//         def down = genFace(3)
-//         def left = genFace(4)
-//     }
+    object New {
+        def Section(label: String): Unit = {
+            section_number = sections.indexOf(label) + 1
+            line_number = 0
+        }
+    }
 
-//     def Face = new FaceClass(section_number, line_number) //TODO increment?
+    object Do {
+        def Section(label: String): Unit = {
+            line_number = line_number + 1
+            val new_section = sections.indexOf(label) + 1
+            rules = rules ++ List(
+                Rule(State(section_number + "0" + line_number + "01"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(new_section + "0101")
+                    ),
+                Rule(State(section_number + "0" + line_number + "02"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(new_section + "0102")
+                    ),
+                Rule(State(section_number + "0" + line_number + "03"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(new_section + "0103")
+                    ),
+                Rule(State(section_number + "0" + line_number + "04"),
+                    Surroundings(Anything, Anything, Anything, Anything),
+                    StayHere,
+                    State(new_section + "0104")
+                    )
+                )
+        }
+    }
 
-// }
 
-trait Turn extends Command {
 
 }
 
-trait Go extends Command {
-
-}
-
-trait Do extends Command {
-
-}
-
-trait Section {
-
-    // implicit def String2Section(label: String): Section = new Section
-}
 
 
 // Stretch Goals:
